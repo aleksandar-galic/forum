4ea2f697f5fa8dfb64f9379b5b0de58db0da1b84
Aleksandar Galic <acagali97@gmail.com>
2020-12-25 13:37:31 +0100

    a thread can have replies

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -1,3 +1,14 @@
 <h2>{{ $thread->title }}</h2>
 
 {{ $thread->body }}
+
+<h4>REPLIES</h4>
+@foreach($thread->replies as $reply)
+    {{ $reply->body }}
+    <br>
+    <a href="#">
+        {{ $reply->owner->name }}
+    </a> said {{ $reply->created_at->diffForHumans() }}...
+    <br>
+    <hr>
+@endforeach
